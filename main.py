1d779c744a4e1ec6ea80828b0664e4c8e5de00bc
charalampidi-garbriella <gcharalampidi@nevada.unr.edu>
2023-10-15 08:48:51 -0700

    sharpening

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from image import PGMImage
 from correlation import correlate
-from gaussian import apply_smoothing, gaussian_mask_7x7, gaussian_mask_15x15
+from gaussian import apply_smoothing
+from sharpening import unsharp_masking, high_boost_filtering
 
 def get_user_choice():
     print("Which function do you want to test?")
     print("1. Correlation")
     print("2. Gaussian Smoothing with 7x7 mask")
     print("3. Gaussian Smoothing with 15x15 mask")
-    
+    print("4. Unsharp Masking")
+    print("5. High-Boost Filtering")
+
     choice = input("Enter the number of your choice: ")
     return choice
 
-
-
 choice = get_user_choice()
+image = PGMImage()
+image.read_from_file('images/lenna.pgm')
 
 if choice == "1":
-    image = PGMImage()
-    image.read_from_file('images/Image.pgm')
     small_image = PGMImage()
     small_image.read_from_file('images/Pattern.pgm')
     mask = small_image.pixels
@@ -31,6 +32,19 @@ elif choice == "2":
 
 elif choice == "3":
     apply_smoothing('images/lenna.pgm', '15x15')
+
+elif choice == "4":
+    sharpened_pixels = unsharp_masking(image.pixels)
+    sharpened_image = PGMImage(width=image.width, height=image.height, maxval=image.maxval)
+    sharpened_image.pixels = sharpened_pixels
+    sharpened_image.write_to_file('images/lenna_unsharp.pgm')
+
+elif choice == "5":
+    k = float(input("Enter the value for K: "))
+    enhanced_pixels = high_boost_filtering(image.pixels, k)
+    enhanced_image = PGMImage(width=image.width, height=image.height, maxval=image.maxval)
+    enhanced_image.pixels = enhanced_pixels
+    enhanced_image.write_to_file(f'images/lenna_high_boost_k{k}.pgm')
 
 else:
     print("Invalid choice.")
